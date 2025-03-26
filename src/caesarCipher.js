e54ec8160091ea9c349ee2c4968ec45d2462ee49
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-03-26 19:50:27 +0530

    feat - implement caesar cipher and write its test cases

---
+++ src/caesarCipher.js
@@ -0,0 +1,39 @@
+//alphabet
+const alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+//utility function
+function isNotLetter(char) {
+    return /[^a-zA-Z]/.test(char);
+}
+
+// caesar cipher func
+
+function caesarCipher(string, shiftFactor) {
+  let caesarCipherString = "";
+  const arrayOfString = string.split("");
+
+  arrayOfString.forEach((letter) => {
+    let indexOfLetter = alphabets.indexOf(letter.toLowerCase());
+    indexOfLetter = indexOfLetter + shiftFactor;
+
+    if (indexOfLetter > alphabets.length - 1) {
+      indexOfLetter = indexOfLetter - alphabets.length;
+    }
+
+    if(isNotLetter(letter) === true) {
+        caesarCipherString =
+        caesarCipherString + letter;
+        return;
+    }
+
+    if (letter.toUpperCase() === letter) {
+      caesarCipherString =
+        caesarCipherString + alphabets[indexOfLetter].toUpperCase();
+    } else {
+      caesarCipherString = caesarCipherString + alphabets[indexOfLetter];
+    }
+  });
+  return caesarCipherString;
+}
+
+export { caesarCipher };
